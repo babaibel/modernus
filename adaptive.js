356191d037429d87a24d04c3cd79f4723c641a62
Babai <31babaev@gmail.com>
2018-06-13 11:00:46 +0300

    js top

--- adaptive.js
+++ adaptive.js
@@ -1,4 +1,18 @@
-$(function() {
+$(function () {
 
-	
+	$('.js-mob-menu').click(function() {
+		$('body').toggleClass('_open-menu').removeClass('_open-search _open-catalog');
+		return false;
+	});
+
+	$('.js-mob-search').click(function() {
+		$('body').toggleClass('_open-search').removeClass('_open-menu _open-catalog');
+		return false;
+	});
+
+	$('.js-mob-catalog').click(function() {
+		$('body').toggleClass('_open-catalog').removeClass('_open-menu _open-search');
+		return false;
+	});
+
 });
